b9c6b0abcd7ccc034546b95b9b0e0e77b794566b
Andrew Mahoney <andrew.mahoney.mswdev@gmail.com>
2021-11-07 22:55:44 +1300

    Added evolution\Demo class

---
+++ evolution/Demo.java
@@ -0,0 +1,38 @@
+package evolution;
+
+import genotype.Genome;
+import genotype.Phenome;
+import main.Settings;
+
+public abstract class Demo {
+    public static void main(String... args) {      
+        System.out.println("Seeding initial population...");
+        Population.initialise();
+
+        int generation = 0;
+        do {
+            System.out.println("Generation " + generation);
+            Evaluate.go();
+
+            int highest, lowest;
+            highest = Population.getNetworks().get(0).getFitness();
+            lowest = Population.getNetworks().get(Population.SIZE-1).getFitness();
+            
+            System.out.println(lowest + "-" + highest);
+            if (highest == Settings.TASKS_FOR_OPTIMAL) {
+                Genome genome = Population.getNetworks().get(0).getGenome();
+                System.out.println(genome);
+                Phenome phenome = new Phenome(genome);
+                Settings.TASK.next();
+                phenome.putInputs(Settings.TASK.getInputs());
+                phenome.run();
+                System.out.println(phenome);
+                System.exit(0);
+            }
+            
+            Reproduce.go();
+            generation++;
+        } while (true);
+    }
+
+}
